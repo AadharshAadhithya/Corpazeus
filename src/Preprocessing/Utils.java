d003b0cf0a3429cd9b81bee21eb7aa1802e623b4
aadharsh <aadharsh@AadharshAadhithya>
2021-02-01 12:29:00 +0530

    Utils.readFile

--- src/Preprocessing/Utils.java
+++ src/Preprocessing/Utils.java
@@ -1,5 +1,23 @@
 package Preprocessing;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Utils {
-
+	
+	public  String readFile(String path) throws FileNotFoundException {
+		File Corpus = new File(path);
+		Scanner scan = new Scanner(Corpus);
+		
+		String raw = "";
+		
+		while(scan.hasNextLine()) {
+			raw = raw.concat(scan.nextLine() + "\n");
+		}
+		
+		scan.close();
+		
+		return raw;
+	}
+	
 }
